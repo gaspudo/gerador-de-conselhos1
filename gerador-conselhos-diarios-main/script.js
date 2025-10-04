585a3825fbda177f049a8cd308ac8a13b6615795
Joao Gaspar <danjpjcisalh@gmail.com>
2025-10-04 18:54:00 -0300

    fix: corrige o codigo e reativa a funcionalidade

--- gerador-conselhos-diarios-main/script.js
+++ gerador-conselhos-diarios-main/script.js
@@ -5,4 +5,12 @@ const conselhos = [
     "Supere seus limites.",
     "Tenha objetivos claros e lute por eles."
 ];
-console.log("Arquivo de script carregado. array de conselhos pronto.");
+const conselhoTexto = document.getElementById('conselhoTexto');
+const novoConselhoBtn = document.getElementById('novoConselhoBtn');
+
+function gerarConselho() {
+    //bug corrigido, removemos o + 1
+    const indice = Math.floor(Math.random() * conselhos.length);
+    conselhoTexto.textContent = conselhos[indice]; 
+}
+novoConselhoBtn.addEventListener('Click', gerarConselho);
