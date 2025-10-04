6d3fb2932c5ddcf3a1a4dd13336c0bdb2b3f446f
Joao Gaspar <danjpjcisalh@gmail.com>
2025-10-04 18:41:27 -0300

    feat: adiciona interatividade ao botao de conselhos

--- gerador-conselhos-diarios-main/script.js
+++ gerador-conselhos-diarios-main/script.js
@@ -5,4 +5,12 @@ const conselhos = [
     "Supere seus limites.",
     "Tenha objetivos claros e lute por eles."
 ];
-console.log("Arquivo de script carregado. array de conselhos pronto.");
+const conselhoTexto = document.getElementById('conselhoTexto');
+const novoConselhoBtn = document.getElementById('novoConselhoBtn');
+
+function gerarConselho() {
+    //bug proposital
+    const indice = Math.floor(Math.random() * (conselhos.length + 1));
+    conselhoTexto.textContent = conselhos(indice);
+}
+novoConselhoBtn.addEventListener('click', gerarConselho);
