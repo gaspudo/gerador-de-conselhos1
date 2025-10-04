024bba10b452429c9d4cf170e112e3748cfe1df9
Joao Gaspar <danjpjcisalh@gmail.com>
2025-10-04 18:30:36 -0300

    criação e upload do READ.ME e demais arquivos

---
+++ gerador-conselhos-diarios-main/script.js
@@ -0,0 +1,8 @@
+const conselhos = [
+    "Acredite em voce mesmo.",
+    "O primeiro passo é o mais importante.",
+    "Seja sua melhor versão!",
+    "Supere seus limites.",
+    "Tenha objetivos claros e lute por eles."
+];
+console.log("Arquivo de script carregado. array de conselhos pronto.");
